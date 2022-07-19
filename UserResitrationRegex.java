ce5aef11557b8648c9c2f7a3598d858d91ad1aef
BhaktiRohane2404 <98884739+BhaktiRohane2404@users.noreply.github.com>
2022-07-19 12:14:40 +0530

    Add files via upload

---
+++ UserResitrationRegex.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.regex;
+
+public class UserResitrationRegex {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the user registration system");
+        UserOperator userOperator = new UserOperator();
+        userOperator.firstName();
+        userOperator.lastName();
+        userOperator.emailAddress();
+        userOperator.mobileNumber();
+        userOperator.password();
+    }
+}
